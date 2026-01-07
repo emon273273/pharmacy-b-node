00689697af8035dcc54578eaf229d1ee6cd4d1e2
emon273273 <emon.cse273@gmail.com>
2026-01-07 22:53:48 +0600

    feat: Implement bulk medicine creation via Excel upload, refactor single medicine creation, and enforce unique supplier names.

--- src/routes/medicine/medicine.controller.ts
+++ src/routes/medicine/medicine.controller.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from "express";
 import getPagination from "../../utils/paginateQuery";
+import XLSX from "xlsx";
 import prisma from "../../utils/prisma";
 import { createMedicineSchema } from "../../utils/schema/createMedicineSchema";
+import { object } from "zod";
 
 interface MedicineQuery {
   query?: string;
@@ -10,9 +12,60 @@ interface MedicineQuery {
   count?: string;
 }
 
-export const createMedicine = async (req: Request, res: Response): Promise<void> => {
+interface MedicineRow {
+  medicineName: string;
+  genericName: string;
+  brandName: string;
+  description: string;
+  dosageType: string;
+  unitType: string;
+  reorderLevel: number;
+  categoryName: string;
+  supplierName: string;
+  batchNumber: string;
+  quantity: number;
+  manufacturingDate: string; 
+  expiryDate: string;      
+  purchasePrice: number;
+  sellingPrice: number;
+}
+
+export const createMedicine =async (req:Request,res:Response):Promise<void>=>{
+  try {
+
+    const queryType=req.query.query as string;
+    
+    if(queryType==='single'){
+
+      await createSingleMedicine(req,res);
+      return;
+    }
+
+    if (queryType==='createMany'){
+
+      await createBulkMedicine(req,res);
+      return;
+    }
+
+    res.status(400).json({
+      status:false,
+      message:"Invalid query type",
+    })
+    
+  } catch (error) {
+    
+    res.status(500).json({
+      status:false,
+      message:"Server error",
+      error:(error as Error).message,
+    })
+  }
+}
+
+ const createSingleMedicine = async (req: Request, res: Response): Promise<void> => {
   try {
     // 1️⃣ Validate input
+
     const parsed = createMedicineSchema.safeParse(req.body);
 
     if (!parsed.success) {
@@ -78,6 +131,169 @@ export const createMedicine = async (req: Request, res: Response): Promise<void>
     });
   }
 };
+
+
+export const createBulkMedicine = async (req: Request, res: Response): Promise<void> => {
+  try {
+    
+
+    if(!req.file){
+
+      res.status(400).json({
+        status:false,
+        message:"No file uploaded",
+      })
+      return;
+    }
+
+    // step 1 : Read The Excel 
+    const workbook =XLSX.read(req.file.buffer,{type:"buffer"});
+    const sheet=workbook.Sheets[workbook.SheetNames[0]];
+    const rows = XLSX.utils.sheet_to_json<MedicineRow>(sheet);
+
+    if(!rows.length){
+
+      res.status(400).json({
+        status:false,
+        message:"No data found in the file",
+      })
+      return;
+    }
+
+    // step 2 : Extract Unique Category and Supplier Names
+    const uniqueCategoryNames=[...new Set(rows.map(r=>r.categoryName).filter(Boolean))];
+    const uniqueSupplierNames=[...new Set(rows.map(r=>r.supplierName).filter(Boolean))];
+
+    // step 3 fetch categories and suppliers
+    const categories=await prisma.category.findMany({
+
+      where:{
+        name:{in:uniqueCategoryNames}
+      },
+      select:{id:true,name:true}
+    });
+
+
+    const suppliers=await prisma.supplier.findMany({
+
+      where:{
+        name:{in:uniqueSupplierNames}
+      },
+      select:{id:true,name:true}
+    });
+
+
+
+    //step 4 create map for easy access name -> id
+
+    const categoryMap=new Map(categories.map(c=>[c.name,c.id]));
+    const supplierMap=new Map(suppliers.map(s=>[s.name,s.id]));
+
+    // step 5 validate : check if any category or supplier is missing is missing from db
+
+    const missingCategories=uniqueCategoryNames.filter(name=>!categoryMap.has(name));
+    const missingSuppliers=uniqueSupplierNames.filter(name=>!supplierMap.has(name));
+
+    if(missingCategories.length>0 || missingSuppliers.length>0){
+
+      res.status(400).json({
+        status:false,
+        message:"Missing categories or suppliers",
+        missingCategories,
+        missingSuppliers,
+      })
+      return;
+    }
+
+
+    // step 6 create medicine 
+    const medicineMap:Record<string,any>={}
+
+    for (const row of rows){
+
+       const key = `${row.medicineName}_${row.brandName}`;
+
+       const categoryId=categoryMap.get(row.categoryName);
+       const supplierId=supplierMap.get(row.supplierName);
+
+       if(!medicineMap[key]){
+
+        medicineMap[key]={
+
+          medicineName:row.medicineName,
+          genericName:row.genericName,
+          brandName:row.brandName,
+          description:row.description,
+          dosageType:row.dosageType,
+          unitType:row.unitType,
+          reorderLevel:row.reorderLevel,
+          categoryId,
+          supplierId,
+          batches:[]
+        };
+       }
+
+       medicineMap[key].batches.push({
+        batchNumber:row.batchNumber,
+        quantity:row.quantity,
+        manufacturingDate:row.manufacturingDate,
+        expiryDate:row.expiryDate,
+        purchasePrice:row.purchasePrice,
+        sellingPrice:row.sellingPrice,
+       })
+    
+       }
+    
+
+       //step 7 insert into database
+
+      const medicineToCreate=Object.values(medicineMap);
+
+
+      // step 8 use transaction to ensure all or nothing success 
+
+      const result=await prisma.$transaction(async (tx)=>{
+
+        const createMedicine=[];
+
+        for (const meddata of medicineToCreate){
+
+          const {batches,...medicineFields}=meddata;
+
+          const newMed =await tx.medicine.create({
+
+            data:{
+              ...medicineFields,
+              batches:{
+                create:batches
+              } 
+            }
+            
+          });
+          createMedicine.push(newMed);
+          
+        }
+        return createMedicine;
+      })
+
+      res.status(201).json({
+        message:"Medicine created successfully",
+        createdMedicine:result,
+      })
+    
+  } catch (error) {
+    const err=error as Error;
+    console.error("Create Bulk Medicine Error:",err);
+    res.status(500).json({
+      status:false,
+      message:"Server error",
+      error:err.message,
+    })
+  }
+}
+
+
+
 
 export const getAllMedicine = async (req: Request, res: Response): Promise<void> => {
   try {
--- src/routes/medicine/medicine.routes.ts
+++ src/routes/medicine/medicine.routes.ts
@@ -1,10 +1,13 @@
 import express, { Router } from "express";
+import multer from "multer"
 import { protectedRoutes } from "../../middleware/protectedRoutes";
-import { createMedicine, getAllMedicine } from "./medicine.controller";
+import { createMedicine, getAllMedicine,  } from "./medicine.controller";
 
+
+const upload=multer({storage:multer.memoryStorage()})
 const medicineRoutes: Router = express.Router();
 
 medicineRoutes.get("/", protectedRoutes, getAllMedicine);
-medicineRoutes.post("/", protectedRoutes, createMedicine);
+medicineRoutes.post("/", protectedRoutes, upload.single("file"), createMedicine);
 
 export default medicineRoutes;
